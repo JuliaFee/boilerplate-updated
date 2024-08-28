0742c9c523ab9330ff5b1f48ca943e245500b73b
JuliaFee <julia.silva133@aluno.senai.br>
2024-08-28 15:28:17 -0300

    ieieieie

--- src/routes/stack.routes.jsx
+++ src/routes/stack.routes.jsx
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Home from "../screens/Home";
 import Totoro from "../screens/Totoro";
+import Ghibli from "../screens/Ghibli";
 
 const Stack = createNativeStackNavigator();
 const StackRoutes = () => {
@@ -11,6 +12,8 @@ const StackRoutes = () => {
             component={Home} />
                 <Stack.Screen name="Totoro" 
                 component={Totoro} />
+         <Stack.Screen name="Ghibli" 
+                component={Ghibli} />
                    
         </Stack.Navigator>
     )
--- src/screens/Ghibli/index.jsx
+++ src/screens/Ghibli/index.jsx
@@ -3,12 +3,12 @@ import { View, Text, TouchableOpacity } from 'react-native'
 import styles from './styles'
 import MyButton from '../../components/MyButton'
 import Title from '../../components/Title'
-export default function Totoro() {
+export default function Ghibli() {
   return (
     <View style={styles.container}>
-      <Title text="Home" />
+      <Title text="Ghibli" />
       <MyButton screen="Totoro" name="Go to Totoro" />
-      <MyButton screen="Ghibli" name="Go to Ghibli" />
+      <MyButton screen="Home" name="Go to Home" />
     </View>
   )
 }
