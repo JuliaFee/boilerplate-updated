9ca4de6a445796c3e14e62db56fd9aa5cf131f29
JuliaFee <julia.silva133@aluno.senai.br>
2024-08-28 12:56:56 -0300

    oi

--- src/screens/Totoro/index.jsx
+++ src/screens/Totoro/index.jsx
@@ -1,10 +1,14 @@
-import { View, Text } from 'react-native'
-
+import { View, Text, TouchableOpacity } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
 
 export default function Totoro() {
+    const navigation = useNavigation()
   return (
     <View>
       <Text>Tonari no Totoro</Text>
+      <TouchableOpacity onPress={() => navigation.navigate('Home')}>
+        <Text>Home</Text>
+      </TouchableOpacity>
     </View>
   )
 }
